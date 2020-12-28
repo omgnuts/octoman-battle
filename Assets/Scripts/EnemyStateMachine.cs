12381128380ecd41e11a478b8b760b4b74f0a6ba
omgnuts <peanuts@munchpress.com>
2020-12-29 03:18:26 +0800

    update scripts

--- Assets/Scripts/EnemyStateMachine.cs
+++ Assets/Scripts/EnemyStateMachine.cs
@@ -6,15 +6,51 @@ public class EnemyStateMachine : MonoBehaviour
 {
     public BaseEnemy enemy; 
 
-    // Start is called before the first frame update
-    void Start()
+    public enum TurnState
     {
-        
+        Processing, 
+        AddToList, 
+        Waiting, 
+        Selecting, 
+        Action, 
+        Dead
+    }
+
+    public TurnState currentState;
+
+    private float currCooldown = 0f;
+    private float maxCooldown = 5f;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        currentState = TurnState.Processing;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        switch (currentState)
+        {
+            case (TurnState.Processing):
+                updateProgressBar();
+                break;
+            case (TurnState.AddToList): break;
+            case (TurnState.Waiting): break;
+            case (TurnState.Selecting): break;
+            case (TurnState.Action): break;
+            case (TurnState.Dead): break;
+            default: break;
+        }
+    }
+
+    void updateProgressBar()
+    {
+        currCooldown += Time.deltaTime;
         
+        if (currCooldown >= maxCooldown)
+        {
+            currentState = TurnState.AddToList;
+        }
     }
 }
--- Assets/Scripts/HeroStateMachine.cs
+++ Assets/Scripts/HeroStateMachine.cs
@@ -1,20 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 
 public class HeroStateMachine : MonoBehaviour
 {
     public BaseHero hero;
     
-    // Start is called before the first frame update
-    void Start()
+    public enum TurnState
     {
-        
+        Processing, 
+        AddToList, 
+        Waiting, 
+        Selecting, 
+        Action, 
+        Dead
+    }
+
+    public TurnState currentState;
+
+    private float currCooldown = 0f;
+    private float maxCooldown = 5f;
+
+    public Image ProgressFG;
+     
+    // Start is called before the first frame update
+    private void Start()
+    {
+        currentState = TurnState.Processing;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        switch (currentState)
+        {
+            case (TurnState.Processing):
+                updateProgressBar();
+                break;
+            case (TurnState.AddToList): break;
+            case (TurnState.Waiting): break;
+            case (TurnState.Selecting): break;
+            case (TurnState.Action): break;
+            case (TurnState.Dead): break;
+            default: break;
+        }
+    }
+
+    void updateProgressBar()
+    {
+        currCooldown += Time.deltaTime;
+        var ratio = currCooldown / maxCooldown;
         
+        ProgressFG.transform.localScale = new Vector3(Mathf.Clamp(ratio, 0f, 1f),
+            ProgressFG.transform.localScale.y, ProgressFG.transform.localScale.z);
+
+        if (currCooldown >= maxCooldown)
+        {
+            currentState = TurnState.AddToList;
+        }
     }
 }
