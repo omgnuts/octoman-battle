da770811e1976688e683fbec78d937b3ca67682a
omgnuts <peanuts@munchpress.com>
2020-12-29 00:52:25 +0800

    init

---
+++ Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,35 @@
+﻿[System.Serializable]
+public class BaseEnemy 
+{
+    public string name;
+
+    public enum Type
+    {
+        Grass,
+        Fire,
+        Water,
+        Electric
+    }
+
+    public enum Rarity
+    {
+        Common, 
+        Uncommon, 
+        Rare, 
+        SuperRare
+    }
+
+    public Type enemyType;
+    public Rarity rarityType;
+
+    public float baseHp;
+    public float currHp;
+    public float baseMp;
+    public float currMp;
+
+    public float baseAtk;
+    public float currAtk;
+    public float baseDef;
+    public float currDef; 
+    
+}
---
+++ Assets/Scripts/BaseHero.cs
@@ -0,0 +1,17 @@
+﻿[System.Serializable]
+public class BaseHero 
+{
+    public string name;
+
+    public float baseHp;
+    public float currHp;
+    public float baseMp;
+    public float currMp;
+
+    public int stamina;
+    public int intellect;
+    public int dexterity;
+    public int agility; 
+
+    
+}
